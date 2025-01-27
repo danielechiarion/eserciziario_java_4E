e784862c8ed3ffe10e13aa75dc6fda43a5326619
danielechiarionù <daniele.chiarion@iisviolamarchesini.edu.it>
2025-01-27 21:20:41 +0100

    completed ex 16

--- Chiarion_4E_Es16A_pizze/src/Main.java
+++ Chiarion_4E_Es16A_pizze/src/Main.java
@@ -8,12 +8,12 @@ public class Main {
         final double pesoPomodoro = 1.4;
         final int numPizzaMargherita = 4;
         final int numPizzaProsciutto = 8;
-        final double pesoPizza = (double)(pesoProsciutto+pesoPomodoro+pesoFarina) / (numPizzaProsciutto+numPizzaMargherita);
+        final double pesoPizza = (pesoProsciutto + pesoPomodoro + pesoFarina) / (numPizzaProsciutto + numPizzaMargherita);
 
         /* dichiarazione variabili */
         int pizzaMargheritaInput, pizzaProsciuttoInput;
-        double pesoPizzaMargherita, pesoPizzaProsciutto;
-        double pesoProsciuttoCorrente, pesoFarinaCorrente, pesoPomodoroCorrente, pesoTotaleFarina=0, pesoTotalePomodoro=0, pesoTotaleProsciutto=0;
+        double pesoTotaleFarina, pesoTotaleProsciutto, pesoTotalePomodoro;
+        double pesoFarinaCorrente, pesoProsciuttoCorrente, pesoPomodoroCorrente;
 
         /* creazione scanner per l'input dei dati */
         Scanner scanner = new Scanner(System.in);
@@ -24,9 +24,25 @@ public class Main {
         System.out.println("Inserisci il numero di pizze al prosciutto: ");
         pizzaProsciuttoInput = scanner.nextInt();
 
-        /* nel primo ciclo calcolo il numero di pizze al prosciutto */
-        pesoFarinaCorrente = pesoFarina/(numPizzaProsciutto+numPizzaMargherita);
-        pesoProsciuttoCorrente = pesoProsciutto/numPizzaProsciutto;
-        pesoPomodoroCorrente = pesoPomodoro/(numPizzaProsciutto+numPizzaMargherita)-pesoProsciuttoCorrente;
+        /* calcolo i pesi per ogni tipo di pizza */
+        pesoFarinaCorrente = pesoFarina / (numPizzaProsciutto + numPizzaMargherita);
+        pesoProsciuttoCorrente = pesoProsciutto / numPizzaProsciutto;
+        pesoPomodoroCorrente = pesoPomodoro / (numPizzaProsciutto + numPizzaMargherita);
+
+        // Compensazione della mancanza di prosciutto nelle pizze margherita
+        double pesoPomodoroMargherita = pesoPomodoroCorrente + pesoProsciuttoCorrente / 2;
+        double pesoPomodoroProsciutto = pesoPomodoroCorrente - pesoProsciuttoCorrente / 2;
+
+        /* stampa i pesi degli ingredienti per ogni pizza */
+        System.out.printf("Peso di riferimento pizza: %.2fkg\n", pesoPizza);
+        System.out.printf("Peso pizza margherita: %.2fkg farina, %.2fkg pomodoro\n", pesoFarinaCorrente, pesoPomodoroMargherita);
+        System.out.printf("Peso pizza prosciutto: %.2fkg farina, %.2fkg prosciutto, %.2fkg pomodoro\n", pesoFarinaCorrente, pesoProsciuttoCorrente, pesoPomodoroProsciutto);
+
+        /* calcolo i pesi totali degli ingredienti */
+        pesoTotaleFarina = pesoFarinaCorrente * (pizzaMargheritaInput + pizzaProsciuttoInput);
+        pesoTotaleProsciutto = pesoProsciuttoCorrente * pizzaProsciuttoInput;
+        pesoTotalePomodoro = pesoPomodoroMargherita * pizzaMargheritaInput + pesoPomodoroProsciutto * pizzaProsciuttoInput;
+
+        System.out.printf("Totale farina: %.2fkg\tTotale pomodoro: %.2fkg\tTotale prosciutto: %.2fkg\n", pesoTotaleFarina, pesoTotalePomodoro, pesoTotaleProsciutto);
     }
 }
